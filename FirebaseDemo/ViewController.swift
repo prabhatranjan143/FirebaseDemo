91181c0caf43889d9d4cb7939cdf85aa408152f9
prabhatranjan143 <prbp143@gmail.com>
2018-11-25 19:56:11 +0530

    Initial Commit

---
+++ FirebaseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseDemo
+//
+//  Created by Prabhat on 25/11/18.
+//  Copyright © 2018 Prabhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
